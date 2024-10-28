5637596bbd9555852da5f83bad88e270c1348adb
Cecilia Ramírez <cramirez25@ucol.mx>
2024-10-28 16:30:47 -0600

    functionality to delete an existing author via delete method with their id

--- src/routes/author.routes.js
+++ src/routes/author.routes.js
@@ -7,5 +7,6 @@ router.get("/", authorController.getAuthors);
 router.get("/:id", authorController.getAuthor);
 router.post("/", authorController.addAuthors);
 router.put("/:id", authorController.updateAuthor);
+router.delete("/:id", authorController.deleteAuthor);
 
 export default router;
